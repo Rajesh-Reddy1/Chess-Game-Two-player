fed407325c6312114544caba2b72a369d88a9600
Rajesh-Reddy1 <mrrajeshreddy1@gmail.com>
2023-02-25 16:29:43 +0530

    pawn

--- chessEngine.py
+++ chessEngine.py
@@ -2,7 +2,7 @@
 
 class game():
     def __init__(self):
-        self.board=[
+        self.board = [
             ['bR','bN','bB','bQ','bK','bB','bN','bR'],
             ['bp','bp','bp','bp','bp','bp','bp','bp'],
             ['--','--','--','--','--','--','--','--'],
@@ -11,37 +11,45 @@ class game():
             ['--','--','--','--','--','--','--','--'],
             ['wp','wp','wp','wp','wp','wp','wp','wp'],
             ['wR','wN','wB','wQ','wK','wB','wN','wR']]
-        self.whiteToMove=True
-        self.moveLog=[]
+        self.whiteToMove = True
+        self.moveLog = []
 
     def MakeMove(self,move):
-        self.board[move.StartRow][move.StartCol]="--"
-        self.board[move.EndRow][move.EndCol]=move.pieceMoved
+        self.board[move.StartRow][move.StartCol] = "--"
+        self.board[move.EndRow][move.EndCol] = move.pieceMoved
         self.moveLog.append(move)
-        self.whiteToMove=not self.whiteToMove
+        self.whiteToMove = not self.whiteToMove
     
     def undoMove(self):
-        if len(self.moveLog)!=0:
+        if len(self.moveLog) != 0:
             move=self.moveLog.pop()
-            self.board[move.StartRow][move.StartCol]=move.pieceMoved
-            self.board[move.EndRow][move.EndCol]=move.pieceCaptured
-            self.whiteToMove=not self.whiteToMove
+            self.board[move.StartRow][move.StartCol] = move.pieceMoved
+            self.board[move.EndRow][move.EndCol] = move.pieceCaptured
+            self.whiteToMove = not self.whiteToMove
 
 
-class move():
+class notations():
     #Board values
-    Rows={"1":7,"2":6,"3":5,"4":4,"5":3,"6":2,"7":1,"8":0} 
-    rowsvalue={v:k for k,v in Rows.items()}
-    Cols={"a":0,"b":1,"c":2,"d":3,"e":4,"f":5,"g":6,"h":7}
-    Colsvalue={v:k for k,v in Cols.items()}
+    Rows = {"1":7,"2":6,"3":5,"4":4,"5":3,"6":2,"7":1,"8":0} 
+    rowsvalue = {v:k for k,v in Rows.items()}
+    Cols = {"a":0,"b":1,"c":2,"d":3,"e":4,"f":5,"g":6,"h":7}
+    Colsvalue = {v:k for k,v in Cols.items()}
     
     def __init__(self,StartSq,EndSq,board):
-        self.StartRow=StartSq[0]
-        self.StartCol=StartSq[1]
-        self.EndRow=EndSq[0]
-        self.EndCol=EndSq[1]
-        self.pieceMoved=board[self.StartRow][self.StartCol]
-        self.pieceCaptured=board[self.EndRow][self.EndCol]
+        self.StartRow = StartSq[0]
+        self.StartCol = StartSq[1]
+        self.EndRow = EndSq[0]
+        self.EndCol = EndSq[1]
+        self.pieceMoved = board[self.StartRow][self.StartCol]
+        self.pieceCaptured = board[self.EndRow][self.EndCol]
+        self.moveID = self.StartRow *1000 + self.StartCol*100 + self.EndRow*10 + self.EndCol
+        print(self.moveID)
+
+    def __eq__(self,other):
+        if isinstance(other,notations):
+            return self.moveID == other.moveID
+            
+        
     
     def Notation(self):
         return self.getRankFile(self.StartRow,self.StartCol)+" "+self.getRankFile(self.EndRow,self.EndCol)
